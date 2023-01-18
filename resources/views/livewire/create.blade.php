1bd8bb1d43b383e6419f959db4bcdff7b353cb99
paulatit <phoekyawatit@gmail.com>
2023-01-18 16:41:25 +0700

    update init code

---
+++ resources/views/livewire/create.blade.php
@@ -0,0 +1,13 @@
+<form>
+    <div class="form-group">
+        <label for="exampleFormControlInput1">Title:</label>
+        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Enter Title" wire:model="title">
+        @error('title') <span class="text-danger">{{ $message }}</span>@enderror
+    </div>
+    <div class="form-group">
+        <label for="exampleFormControlInput2">Content:</label>
+        <textarea type="email" class="form-control" id="exampleFormControlInput2" wire:model="content" placeholder="Enter Content"></textarea>
+        @error('content') <span class="text-danger">{{ $message }}</span>@enderror
+    </div>
+    <button wire:click.prevent="store()" class="btn btn-success">Save</button>
+</form>
